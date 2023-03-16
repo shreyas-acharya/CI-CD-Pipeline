f32ad7c5a66db3711ab1e8a3576e3bab5950cac7
shreyas-acharya <acharyashreyas14@gmail.com>
2023-03-16 14:57:08 +0530

    Update check_if_port_up.py file

--- check_if_port_up.py
+++ check_if_port_up.py
@@ -9,6 +9,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 for _ in range(10):
     result = sock.connect_ex(("0.0.0.0", 8000))
     if result == 0:
+        print("Port is up")
         sys.exit(0)
     else:
         print("waiting ...")
